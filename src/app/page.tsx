e522e89358ccea4680511a714674a4442451c524
brkcinar <brkcinar@hotmail.com.tr>
2025-01-12 22:35:25 +0100

    arka plan düzeltme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,12 +21,12 @@ export default function Home() {
 
     const interval = setInterval(() => {
       if (index < fullText.length) {
-        setDisplayText((prev) => prev + fullText[index]);
+        setDisplayText(fullText.substring(0, index+1));
         index++;
       } else {
         clearInterval(interval); // Stop the animation when complete
       }
-    }, 50); // 50ms per character
+    }, 30); // 30ms per character
 
     return () => clearInterval(interval);
   }, []);
